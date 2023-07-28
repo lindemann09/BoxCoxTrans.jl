a71a01aa1fc9ce6f487753140ce6c36f65fb80f7
Oliver Lindemann <lindemann09@googlemail.com>
2023-07-28 15:15:49 +0200

    show, propertynames

--- src/struct.jl
+++ src/struct.jl
@@ -5,8 +5,31 @@ struct BoxCoxTransformation{T <: AbstractFloat, N <: Real}
     dat::Vector{N}
 end
 
+Base.propertynames(::BoxCoxTransformation) =
+            (:lambda, :dat, :method, :details, :loglik, :transformed)
+
+function Base.getproperty(bc::BoxCoxTransformation, s::Symbol)
+    if s === :lambda || s === :λ
+        return bc.value
+    elseif s === :𝐱
+        return bc.dat
+    elseif s === :transformed
+        return transform(bc.𝐱, bc.λ)
+    elseif s === :loglik
+        return log_likelihood(bc.dat, bc.λ; method = bc.method)
+    else
+        return getfield(bc, s)
+    end
+end
+
+log_likelihood(bc::BoxCoxTransformation) = log_likelihood(bc.dat, bc.λ; method = bc.method)
+log_likelihood(bc::BoxCoxTransformation, λ) = log_likelihood(bc.dat, λ; method = bc.method)
+
+transform(bc::BoxCoxTransformation; kwargs...) = transform(bc.𝐱, bc.λ; kwargs...)
+transform(bc::BoxCoxTransformation, λ::Real; kwargs...) = transform(bc.𝐱, λ; kwargs...)
+
 """
-    BoxCoxTransformation(𝐱::Vector{<:Real};interval = (-2.0, 2.0),
+    BoxCoxTransformation(𝐱::Vector{<:Real}; interval = (-2.0, 2.0),
         method = :geomean)
 
 Create a BoxCoxTransformation. The method wraps `BoxCoxTrans.lambda`` but
@@ -20,24 +43,6 @@ function BoxCoxTransformation(𝐱::Vector{<:Real};
     return BoxCoxTransformation(bc.value, bc.details, method, 𝐱)
 end
 
-function Base.getproperty(bc::BoxCoxTransformation, s::Symbol)
-    if s === :interval
-        return (bc.details.initial_lower, bc.details.initial_upper)
-    elseif s === :lambda || s === :λ
-        return bc.value
-    elseif s === :𝐱
-        return bc.dat
-    else
-        return getfield(bc, s)
-    end
-end
-
-log_likelihood(bc::BoxCoxTransformation) = log_likelihood(bc.dat, bc.λ; method = bc.method)
-log_likelihood(bc::BoxCoxTransformation, λ) = log_likelihood(bc.dat, λ; method = bc.method)
-
-transform(bc::BoxCoxTransformation; kwargs...) = transform(bc.𝐱, bc.λ; kwargs...)
-transform(bc::BoxCoxTransformation, λ::Real; kwargs...) = transform(bc.𝐱, λ; kwargs...)
-
 """
     confint(bc::BoxCoxTransformation; alpha=0.05)
 
@@ -50,9 +55,16 @@ log-likelihood(λ) >= log_likelihood(λ_max) - 0.5 * quantile(Chisq(1), 1-alpha)
 Reference: Linear Models With R by Julian Faraway Section 8.1.
 """
 function confint(bc::BoxCoxTransformation; alpha = 0.05)
+    interval = (bc.details.initial_lower, bc.details.initial_upper)
     τ = log_likelihood(bc, bc.λ) - 0.5 * quantile(Chisq(1), 1 - alpha) # target log-likelihood
     dist_fnc(x) = abs(log_likelihood(bc, x) - τ)
-    res_upper = optimize(dist_fnc, bc.λ, bc.interval[2])
-    res_lower = optimize(dist_fnc, bc.interval[1], bc.λ)
+    res_upper = optimize(dist_fnc, bc.λ, interval[2])
+    res_lower = optimize(dist_fnc, interval[1], bc.λ)
     return (minimizer(res_lower), minimizer(res_upper))
 end
+
+
+function Base.show(io::IO, ::MIME"text/plain", x::BoxCoxTransformation)
+	println(io, "BoxCoxTransformation: λ = " * string(x.value))
+	#println(io, x.details)
+end;
